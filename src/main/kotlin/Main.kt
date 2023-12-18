d1495df21ec17b7b91ee2deb5f79d5d3b2df97a8
Mila <gorbunova.milasha@gmail.com>
2023-12-18 18:51:35 +0200

    Started to implement comunication layer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,43 @@
-fun main(args : Array<String>) {
-    println("Hello, W${add(3,-3)}rld!")
+fun main(args: Array<String>) {
+    println("Hello World!")
+    val output = object : MainOutput {
+        override fun printLine(msg: String) {
+            println(msg)
+        }
+    }
 }
 
-fun add (x: Int, y: Int): Int {
-    return x+y;
+interface MainOutput {
+    fun printLine(msg: String)
+}
+
+interface FileSystem {
+    fun doesFileExist(filePaths: String): Boolean
+    fun readFileAsString(filePaths: String): String
+}
+fun mainHandler(args: Array<String>, output: MainOutput, fs: FileSystem) {
+    if (args.isEmpty()) {
+        output.printLine(Messages.noArgs)
+        return
+    }
+
+    if (!fs.doesFileExist(args[0])) {
+        output.printLine(Messages.inputFileDoesNotExist)
+        return
+    }
+}
+
+object Messages {
+    val noArgs = """
+    Hi, this is tetris game! If you want to play, please, pass an input file. For example:
+    $ tetris input.txt
+  """.trimIndent()
+
+    val inputFileDoesNotExist = """
+        The file you passed does not exist
+    """.trimIndent()
+
+    val fileSyntaxError = """
+        There is syntax error in your input file.
+    """.trimIndent()
 }
