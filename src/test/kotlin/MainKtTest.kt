67e319e555db606b4b6b36a01d1a23b4a6aaebae
svitbar <svitbar1102@gmail.com>
2023-12-19 21:21:17 +0200

    Modify mocks to library ones

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,28 +1,15 @@
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class MainKtTest {
-    val messages = mutableListOf<String>()
-    private val output: MainOutput = object : MainOutput {
-        override fun printLine(msg: String) {
-            messages.add(msg)
-        }
-    }
-
-    private val defaultFsMock = object : FileSystem {
-        override fun doesFileExist(filePaths: String): Boolean {
-            throw RuntimeException("Unexpected call of doesFileExist method")
-        }
-
-        override fun readFileAsStringList(filePaths: String): List<String> {
-            throw RuntimeException("Unexpected call of readFileAsString method")
-        }
-    }
+    private val output: MainOutput = mockk()
+    private val defaultFsMock: FileSystem = mockk()
 
     @BeforeEach
     fun setUp() {
-        messages.clear()
+        clearAllMocks()
+        every { output.printLine(any()) } just Runs
     }
 
     @Test
@@ -30,46 +17,38 @@ class MainKtTest {
         mainHandler(emptyArray(), output, defaultFsMock)
 
         //щоб не було помилки аут оф баундс
-        assertEquals(1, messages.size)
-        assertEquals(Messages.noArgs, messages[0])
+        verify(exactly = 1) { output.printLine(Messages.noArgs) }
     }
 
     @Test
     fun `Should return message if an input file does not exist`() {
-        val fsMock = object : FileSystem {
-            override fun doesFileExist(filePaths: String): Boolean = false
-            override fun readFileAsStringList(filePaths: String): List<String> {
-                throw RuntimeException("Unexpected call of readFileAsString method")
-            }
-        }
+        val fsMock: FileSystem = mockk()
+        every { fsMock.doesFileExist(any()) } returns false
 
         mainHandler(arrayOf("fileDoesNotExist.txt"), output, fsMock)
 
-        assertEquals(1, messages.size)
-        assertEquals(Messages.inputFileDoesNotExist, messages[0])
+        verify(exactly = 1) { output.printLine(Messages.inputFileDoesNotExist) }
     }
 
     @Test
     fun `Should throw an error if input file exists and has a syntax error`() {
-        val fsMock = object : FileSystem {
-            override fun doesFileExist(filePaths: String): Boolean = true
-            override fun readFileAsStringList(filePaths: String): List<String> = """
-                Wrong data
-                Wrong data
-            """.trimIndent().lines()
-        }
+        val fsMock: FileSystem = mockk()
+        every { fsMock.doesFileExist(any()) } returns true
+        every { fsMock.readFileAsStringList(any()) } returns """
+            Wrong data
+            Wrong data
+        """.trimIndent().lines()
 
         mainHandler(arrayOf("input.txt"), output, fsMock)
 
-        assertEquals(1, messages.size)
-        assertEquals(Messages.inputFileContainsWrongData, messages[0])
+        verify(exactly = 1) { output.printLine(Messages.inputFileContainsWrongData) }
     }
 
     @Test
     fun `Should print result field`() {
-        val fsMock = object : FileSystem {
-            override fun doesFileExist(filePaths: String): Boolean = true
-            override fun readFileAsStringList(filePaths: String): List<String> = """
+        val fsMock: FileSystem = mockk()
+        every { fsMock.doesFileExist(any()) } returns true
+        every { fsMock.readFileAsStringList(any()) } returns """
                 7 8
                 ..p.....
                 .ppp....
@@ -79,11 +58,8 @@ class MainKtTest {
                 ...#...#
                 #..#####
             """.trimIndent().lines()
-        }
 
         mainHandler(arrayOf("input.txt"), output, fsMock)
-
-        assertEquals(1, messages.size)
 
         val resultField = """
             ........
@@ -93,7 +69,8 @@ class MainKtTest {
             ..p#....
             ...#...#
             #..#####
-        """.trimIndent();
-        assertEquals(resultField, messages[0])
+        """.trimIndent()
+
+        verify(exactly = 1) { output.printLine(resultField) }
     }
 }
