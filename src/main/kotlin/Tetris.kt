ffa28b20836d83080665782efa7f62e90a8a603a
Mila <gorbunova.milasha@gmail.com>
2023-12-19 22:11:16 +0200

    Added tests for playMultipleFields() method

--- src/main/kotlin/Tetris.kt
+++ src/main/kotlin/Tetris.kt
@@ -10,5 +10,9 @@ class Tetris {
             }
             return finalField
         }
+
+        fun playMultipleFields(field: Field): List<Field> {
+            return listOf(field)
+        }
     }
 }
